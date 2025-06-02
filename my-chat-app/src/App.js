ee5b01c766d3b5a2d62f9b59eb1781d70b096618
Swati-161 <nimswati7@gmail.com>
2025-06-03 02:35:46 +0530

    done changes

---
+++ my-chat-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-chat-app/src/components/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Welcome() {
+  return <h1>👋 Hello World! What do you feel about it?</h1>;
+}
+
+export default Welcome;
